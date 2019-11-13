f67d17dd7dd5686ddb996569239f11c2d7d35683
unknown <Tobias.Throm@gmx.de>
2019-11-13 16:56:54 +0100

    added getInfo, increase Generation and repopulate functions

--- GeneticAlgorithm/population.py
+++ GeneticAlgorithm/population.py
@@ -6,7 +6,7 @@ class Population():
         self.popSize = popSize
         self.indiviuals = []
         for i in range(popSize):
-            self.indiviuals.append(Picture(500))
+            self.indiviuals.append(Picture(5))
     
     def getFittestIdx(self):
         fittest = self.indiviuals[0]
@@ -28,3 +28,27 @@ class Population():
     
     def getIndividual(self, idx):
         return self.indiviuals[idx]
+
+    def getInfo(self):
+        print("Generation:",self.generation)
+        fittest, _ = self.getFittestIdx()
+        print("Fittest:", self.getIndividual(fittest).getFitness())
+        avgFitness = 0
+        for i in self.indiviuals:
+            avgFitness += i.getFitness()
+        avgFitness = avgFitness/self.popSize
+        print("Avg Fitness:", avgFitness)
+    
+    def increaseGeneration(self):
+        self.generation +=1
+
+    def repopulate(self):
+        fittestIdx,secondIdx = self.getFittestIdx()
+        fittest = self.getIndividual(fittestIdx)
+        second = self.getIndividual(secondIdx)
+        length = len(self.indiviuals) 
+        for i in range(length-2):
+            self.indiviuals[i] = fittest.newIndividual(second)
+        self.indiviuals[length-2] = fittest
+        self.indiviuals[length-1] = second
+
