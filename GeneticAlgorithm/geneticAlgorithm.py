0656d1d9b7408e1d4c07f90603af64f610cb12d1
unknown <Tobias.Throm@gmx.de>
2019-11-14 11:50:06 +0100

    added graphs that display the changes over time

--- GeneticAlgorithm/geneticAlgorithm.py
+++ GeneticAlgorithm/geneticAlgorithm.py
@@ -1,5 +1,7 @@
 from population import Population
 import numpy as np
+import matplotlib.pyplot as plt
+
 
 class GeneticAlgorithm():
     def __init__(self,popSize):
@@ -10,15 +12,27 @@ class GeneticAlgorithm():
         #TODO add termination criterion if Fittest is greater than treshhold
         fittest,_ = self.population.getFittestIdx()
         ind = self.population.getIndividual(fittest)
-        print(ind.getPicture())
-        while self.population.generation < 100:
-            self.population.getInfo()
+        ind.display()
+        generation = []
+        fittestVal = []
+        avgFitness = []
+        while self.population.generation < 1000:
+            gen,val,avg=self.population.getInfo()
+            generation.append(gen)
+            fittestVal.append(val)
+            avgFitness.append(avg)
             self.population.repopulate()
             self.population.increaseGeneration()
         fittest,_ = self.population.getFittestIdx()
         ind2 = self.population.getIndividual(fittest)
-        print(ind2.getPicture())
+        ind2.display()
         diff = ind2.getPicture().astype('int32')  - ind.getPicture().astype('int32')  
         print("Changes:")
         print(diff)
         print("Sum of changes:", diff.sum())
+        plt.plot(fittestVal)
+        plt.ylabel('Fitness of fittest Individual')
+        plt.show()
+        plt.plot(avgFitness)
+        plt.ylabel('Average fitness of whole population')
+        plt.show()
